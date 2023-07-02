2287090ee77e627d17efc9bd0703c3b4eb7e88fd
Zak khan <zakaria2012@gmail.com>
2023-07-03 00:39:26 +0100

    scaping characters in url

---
+++ PDO/charScaping.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+** Intermediate PHP concepts
+** PDO CONCEPTS: character scaping with the code
+*/
+
+
+$db = new PDO('mysql:host=localhost; dbname=user', 'root', 'root');
+
+
+if(!empty($_GET("query"))){
+    echo $userId = (int)($_GET["query"]); 
+    $query = $db->query('SELECT * FROM users WHERE id = 1');
+    $result = $query->fetchObject(); 
+    var_dump($result);
+}
