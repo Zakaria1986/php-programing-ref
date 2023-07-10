18e7a2957c19b5671c3e3c05174bfd16cb3b723b
Zak khan <zakaria2012@gmail.com>
2023-07-10 02:49:01 +0100

    pulling out single user record from database

--- mysqli_con/index.php
+++ mysqli_con/index.php
@@ -1,2 +1,12 @@
 <?php
 require_once('./db.php');
+
+$user = $db->query('SELECT * FROM users WHERE id =1');
+
+
+
+
+$user = $user->fetch_assoc(); 
+
+echo '<pre>', var_dump($user['email']) , '</pre>'; 
+echo $user["email"];
