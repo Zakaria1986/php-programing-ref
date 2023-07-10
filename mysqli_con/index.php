778b27f94d2e4604fcb528a4114a2dd0f737a220
Zak khan <zakaria2012@gmail.com>
2023-07-10 03:04:29 +0100

    Pringting out the data on to the DOM, looping through with short hand while loop

--- mysqli_con/index.php
+++ mysqli_con/index.php
@@ -1,12 +1,23 @@
 <?php
 require_once('./db.php');
 
-$user = $db->query('SELECT * FROM users WHERE id =1');
+$user = $db->query('SELECT * FROM users');
+// $user = $user->fetch_assoc(); 
+
+// echo '<pre>', var_dump($user) , '</pre>'; 
+// echo $user["email"];
+
+// while($rows= $user->fetch_assoc()){
+//     echo '<pre>',var_dump($rows), '</pre>';
+// }
+
+while($rows = $user->fetch_assoc()): ?>
+
+<h4>User id: <?php echo $rows['id'] ?></h4>
+<h4>Full Name: <?php echo  $rows['first_name'] .' '. $rows['last_name']?></h4>
+<h4>Email: <?php echo $rows['email']?></h4>
+<h4>User Created: <?php echo $rows['created']?></h4><br>
 
 
 
-
-$user = $user->fetch_assoc(); 
-
-echo '<pre>', var_dump($user['email']) , '</pre>'; 
-echo $user["email"];
+<?php endwhile ?>
