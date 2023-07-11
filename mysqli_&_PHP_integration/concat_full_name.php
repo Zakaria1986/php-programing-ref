bee3ffc08535c86189db8d49bc41a411674dd963
Zak khan <zakaria2012@gmail.com>
2023-07-11 03:36:01 +0100

    Update user

--- mysqli_&_PHP_integration/concat_full_name.php
+++ mysqli_&_PHP_integration/concat_full_name.php
@@ -3,15 +3,18 @@ require_once('./db.php');
 
 
 // Concating names into full name while making the query
-$userCount = $db->query('SELECT id, email, created, CONCAT(first_name," ", last_name) as fullName FROM users');
+$userCount = $db->query('SELECT id, email, created, CONCAT(first_name," ", last_name) as fullName,  FROM users');
 
 // var_dumply the database return data
 echo '<br>', var_dump($userCount);
 
 // Here getting the num of rows which show the number of user, var_dum dumps out objects and object in php are access via arrow '->'
 echo '<br><br>  We have total of ', $userCount->num_rows, " ueser.";
+     
 // Fetching dabase base data as arrays
-while($rows = $userCount->fetch_array()): ?>
+while($rows = $userCount->fetch_array()): 
+
+?>
 
 <h5><?php echo $rows['id'] ?></h5>
 <h5><?php echo $rows['fullName'] ?></h5>
